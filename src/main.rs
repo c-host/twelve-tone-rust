bd1c7795ba19ca336581ec72a127355f95b2f56d
jgart <jgart@dismail.de>
2023-07-05 20:10:43 -0500

    Add basic twelve_tone_row.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    // TODO: Try changing :? to :#? and see the difference.
+    let twelve_tone_row: Vec<u32> = (0..12).collect();
+    println!("{:?}", twelve_tone_row);
 }
