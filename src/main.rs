51b6774d6e50e5de1a3a7a6ddf15cc4d066767b3
jgart <jgart@dismail.de>
2023-07-06 01:25:37 -0500

    Add 12 transpositions.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-fn main() {
-    // TODO: Try changing :? to :#? and see the difference.
-    let twelve_tone_row: Vec<u32> = (0..12).collect();
-    println!("{:?}", twelve_tone_row);
+fn transpose(pitch: &u32, t: u32) -> u32 {
+    (pitch + t) % 12
 }
+
+fn get_transposition(t: u32) -> Vec<u32> {
+    let twelve_tone_row: Vec<u32> = (0..12).collect();
+    let transposed_row: Vec<u32> = twelve_tone_row
+        .iter()
+        .map(|pitch| transpose(pitch, t))
+        .collect();
+    transposed_row
+}
+
+fn main() {
+    for x in 0..12 {
+        println!("{:?}", get_transposition(x));
+    }
+}
+
+// TODO: Add tests.
